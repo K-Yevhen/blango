b58d087150476951da79cce9c22e02efadd89891
Kuts Yevhen  Kuts Yevhen <jipnuygolyb@gmail.com>
2022-12-07 09:00:07 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -88,8 +88,49 @@ class Dev(Configuration):
 
     WSGI_APPLICATION = 'blango.wsgi.application'
 
+    DJANGO_ADMINS="Ben Shaw,ben@example.com;Leo Lucio,leo@example.com"
 
-    # Database
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "filters": {
+            "require_debug_false": {
+                "()": "django.utils.log.RequireDebugFalse",
+            },
+        },
+        "formatters": {
+            "verbose": {
+                "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+                "style": "{",
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "verbose",
+            },
+            "mail_admins": {
+                "level": "ERROR",
+                "class": "django.utils.log.AdminEmailHandler",
+                "filters": ["require_debug_false"],
+            },
+        },
+        "loggers": {
+            "django.request": {
+                # "propagate": True
+                "handlers": ["mail_admins"],
+                "level": "ERROR",
+                "propagate": True,
+            },
+        },
+        "root": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    }
+
+    # Databasex
     # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
     DATABASES = {
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
-from blog.forms import CommentForm
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from blog.models import Post
-from django.shortcuts import render, get_object_or_404
+from blog.forms import CommentForm
+import logging
+from django.urls import reverse
+
+logger = logging.getLogger(__name__)
+
 
 # Create your views here.
 def index(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now())
+    posts = Post.objects.filter(published_at__lte=timezone.now()).select_related("author")
+    logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
-
+  
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
     if request.user.is_active:
@@ -31,4 +35,8 @@ def post_detail(request, slug):
         comment_form = None
     return render(
         request, "blog/post-detail.html", {"post": post, "comment_form": comment_form}
+    )
+def post_table(request):
+    return render(
+        request, "blog/post-table.html", {"post_list_url": reverse("post-list")}
     )
