ef56cc76a81f08cd23d68d55304145e2d371bf30
Kuts Yevhen  Kuts Yevhen <jipnuygolyb@gmail.com>
2022-12-09 13:13:14 +0000

    Finish database optimization

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 def index(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now())
+    posts = Post.objects.filter(published_at__lte=timezone.now()).select_related("author")
     logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
   
@@ -41,3 +41,7 @@ def post_table(request):
     return render(
         request, "blog/post-table.html", {"post_list_url": reverse("post-list")}
     )
+
+def get_ip(request):
+  from django.http import HttpResponse
+  return HttpResponse(request.META['REMOTE_ADDR'])
