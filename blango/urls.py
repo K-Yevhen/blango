1eed9ad5d6151bc0ff2cafd4ae89a056d1c90c2f
Kuts Yevhen  Kuts Yevhen <jipnuygolyb@gmail.com>
2022-12-14 09:02:07 +0000

    Finish django Allauth

--- blango/urls.py
+++ blango/urls.py
@@ -27,17 +27,19 @@ urlpatterns = [
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("ip/", blog.views.get_ip),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("accounts/profile/", blango_auth.views.profile, name="profile"),
-    path(
-    "accounts/register/",
-    RegistrationView.as_view(form_class=BlangoRegistrationForm),
-    name="django_registration_register",
-    ),
-    path("accounts/", include("django_registration.backends.activation.urls")),
+    path("post-table/", blog.views.post_table, name="blog-post-table"),
 ]
 
 if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include(debug_toolbar.urls)),
+        path("accounts/", include("django.contrib.auth.urls")),
+        path("accounts/profile/", blango_auth.views.profile, name="profile"),
+        path(
+        "accounts/register/",
+        RegistrationView.as_view(form_class=BlangoRegistrationForm),
+        name="django_registration_register",
+        ),
+        path("accounts/", include("django_registration.backends.activation.urls")),
+        path("accounts/", include("allauth.urls")),
     ]
