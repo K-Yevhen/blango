0e275274e7995c7a59ac120039141bfa80c6ee04
Kuts Yevhen  Kuts Yevhen <jipnuygolyb@gmail.com>
2023-01-04 09:49:05 +0000

    Finish DRF views

--- blango/urls.py
+++ blango/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("ip/", blog.views.get_ip),
     path("post-table/", blog.views.post_table, name="blog-post-table"),
-    path("api/v1/", include("blog.api_urls")),
+    path("api/v1/", include("blog.api.urls")),
 ]
 
 if settings.DEBUG:
