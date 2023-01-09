abc482df350991ebea41ffb2504bea56469bf085
Kuts Yevhen  Kuts Yevhen <jipnuygolyb@gmail.com>
2023-01-09 08:51:08 +0000

    Finish related fields

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,9 +1,11 @@
 from rest_framework import generics
 
-from blog.api.serializers import PostSerializer
+from blango_auth.models import User
+from blog.api.serializers import PostSerializer, UserSerializer
 from blog.models import Post
 from rest_framework.authentication import SessionAuthentication
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
+
 
 class PostList(generics.ListCreateAPIView):
     authentication_classes = [SessionAuthentication]
@@ -15,3 +17,9 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    lookup_field = "email"
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
