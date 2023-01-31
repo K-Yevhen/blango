05a2d898af556399602b4d8ac64c51cef70657af
Kuts Yevhen  Kuts Yevhen <jipnuygolyb@gmail.com>
2023-01-31 09:11:33 +0000

    Finish JavaScript intro and fundamentals

--- blog/views.py
+++ blog/views.py
@@ -45,3 +45,6 @@ def post_table(request):
 def get_ip(request):
   from django.http import HttpResponse
   return HttpResponse(request.META['REMOTE_ADDR'])
+
+def post_table(request):
+    return render(request, "blog/post-table.html")
