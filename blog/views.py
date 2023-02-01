bd4c0d0b4c1e9205c12c942a2a8ae309b6a9e223
Kuts Yevhen  Kuts Yevhen <jipnuygolyb@gmail.com>
2023-02-01 11:56:49 +0000

    Finish fetch and React hooks

--- blog/views.py
+++ blog/views.py
@@ -47,4 +47,6 @@ def get_ip(request):
   return HttpResponse(request.META['REMOTE_ADDR'])
 
 def post_table(request):
-    return render(request, "blog/post-table.html")
+    return render(
+        request, "blog/post-table.html", {"post_list_url": reverse("post-list")}
+    )
